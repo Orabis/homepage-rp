8f042b3282961281d8b4d99787a3794168fb531a
Léo <cdf.leo.merkel@gmail.com>
2025-04-29 21:27:32 +0200

    added documentation for stockseeker

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        'Documentation ExportGitlab': 'doc-export/index.html',
+        'Documentation StockSeeker': 'doc-stock/index.html',
+      }
+    }
+  },
   server: {
     port: 5001,
     host: "0.0.0.0",
